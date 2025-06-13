67e07fb269098b8ff3c2bf8dc5cb28dff3b6e997
paciaM321 <fifi.paczk@o2.pl>
2025-06-14 01:03:11 +0200

    dodanie klas osoba i pracownik(pola,konstruktor, getery i setery)

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,2 +1,53 @@
-public class Osoba {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Osoba implements Serializable {
+
+    protected String imie;
+
+    protected String nazwisko;
+
+    protected Date dataUrodzenia;
+
+    // gettery, settery, konstruktor, toString()
+
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public Date getDataUrodzenia() {
+        return dataUrodzenia;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setDataUrodzenia(Date dataUrodzenia) {
+        this.dataUrodzenia = dataUrodzenia;
+    }
+
+    public Osoba(String imie, String nazwisko, Date dataUrodzenia) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.dataUrodzenia = dataUrodzenia;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", dataUrodzenia=" + dataUrodzenia +
+                '}';
+    }
 }
--- src/Pracownik.java
+++ src/Pracownik.java
@@ -1,49 +1,42 @@
 import java.io.Serializable;
 import java.util.Date;
-//import javax.xml.bind.annotation.XmlElement;
-//import javax.xml.bind.annotation.XmlRootElement;
 
-public class Pracownik  implements Serializable {
+public class Pracownik extends Osoba implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String imie;
-    private String nazwisko;
+
     private String adres;
     private String zawod;
-    private Date dataUrodzenia;
-
-    public Pracownik() {}
 
     public Pracownik(String imie, String nazwisko, String adres, String zawod, Date dataUrodzenia) {
-        this.imie = imie;
-        this.nazwisko = nazwisko;
+        super(imie, nazwisko, dataUrodzenia);
         this.adres = adres;
         this.zawod = zawod;
-        this.dataUrodzenia = dataUrodzenia;
-    }
-
-    public String getImie() {
-        return imie;
-    }
-
-    public String getNazwisko() {
-        return nazwisko;
     }
 
     public String getAdres() {
         return adres;
     }
 
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
     public String getZawod() {
         return zawod;
     }
 
-    public Date getDataUrodzenia() {
-        return dataUrodzenia;
+    public void setZawod(String zawod) {
+        this.zawod = zawod;
     }
 
+    @Override
     public String toString() {
-        return "Pracownik: " + "imię: " +imie+ ", nazwisko: " +nazwisko+ " ,adres: " +adres+ " , zawód: " +zawod+ ", data urodzenia: " +dataUrodzenia;
+        return "Pracownik: " +
+                "imię: " + imie +
+                ", nazwisko: " + nazwisko +
+                ", adres: " + adres +
+                ", zawód: " + zawod +
+                ", data urodzenia: " + dataUrodzenia;
     }
-
 }
 
